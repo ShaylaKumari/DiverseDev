e9fbb21772cd48d8710aa49a5788c91c1def9aa8
Ananda Shayla <anandashayla@hotmail.com>
2025-11-12 18:59:38 -0300

    Exercício imc

---
+++ aula12_11/imc.js
@@ -0,0 +1,17 @@
+let peso = prompt("Digite seu peso: ");
+let altura = prompt("Digite sua altura: ");
+
+let imc = peso / (altura * altura);
+console.log("Seu IMC é:", imc);
+
+if (imc < 18.5) {
+    console.log("Você está abaixo do peso");   
+} else if (imc < 25) {
+    console.log("Você está no peso normal");
+} else if (imc < 30) {
+    console.log("Você está com sobrepeso");
+} else if (imc < 35) {
+    console.log("Você está com obesidade grau I");
+} else {
+    console.log("Você está com obesidade grau II");
+} 
