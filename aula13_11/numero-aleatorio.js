ecc3356d043889db18a9b3c16f885b501e990dff
Ananda Shayla <anandashayla@hotmail.com>
2025-11-13 16:55:12 -0300

    Exercício do número aleatório

---
+++ aula13_11/numero-aleatorio.js
@@ -0,0 +1,18 @@
+let numeroAleatorio = Math.floor(Math.random() * 101);
+console.log(numeroAleatorio);
+
+let palpite = Number(prompt("Adivinhe o número aleatório entre 0 e 100:"));
+let contador = 1
+
+while(palpite != numeroAleatorio) {
+    if (palpite > numeroAleatorio) {
+        alert("O número digitado é maior que o número aleatório, tente novamente.");
+    } else {
+        alert("O número digitado é menor que o número aleatório, tente novamente.");
+    }
+
+    palpite = Number(prompt("Tente novamente (0 a 100):"));
+    contador++;
+}
+
+alert(`Parabéns! Você acertou o número ${numeroAleatorio}`);
