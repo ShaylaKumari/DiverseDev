7bbbe79b0668e7a2e9cc9213c6cf49c7b9193bc1
Ananda Shayla <anandashayla@hotmail.com>
2025-11-14 19:04:55 -0300

    Exercício produtos

---
+++ aula14_11/produtos.js
@@ -0,0 +1,30 @@
+const produtos = [
+    { nome: "camisa", preco: 80, quantidade: 15 },
+    { nome: "calça", preco: 120, quantidade: 10 },
+    { nome: "tenis", preco: 100, quantidade: 8 },
+    { nome: "meia", preco: 40, quantidade: 5 }
+];
+
+const calcularTotalEstoque = produtos =>
+    produtos.map(p => ({
+        ...p,
+        total: p.preco * p.quantidade
+    }));
+
+const aplicarDesconto = produtos =>
+    produtos.map(p => ({
+        ...p,
+        preco: p.preco * 0.9
+    }));
+
+const filtrarAcimaDe50 = produtos =>
+    produtos.filter(p => p.preco > 50);
+
+const TotalEstoque = calcularTotalEstoque(produtos);
+const produtosComDesconto = aplicarDesconto(produtos);
+const produtosAcimaDe50 = filtrarAcimaDe50(produtosComDesconto);
+
+
+console.log("Preço total do estoque dos produtos:", TotalEstoque);
+console.log("Produtos com desconto:", produtosComDesconto);
+console.log("Produtos acima de R$ 50,00 com desconto:", produtosAcimaDe50);
